af12ab3ecfb30c7a5c163fd50b02ccd6bbea03da
yunan-l <yunan2.lin@gmail.com>
2026-05-01 17:15:54 +0200

    Remove unused function

--- src/physics/variables/output.jl
+++ src/physics/variables/output.jl
@@ -19,7 +19,6 @@ function output_training!(output::Output,
     output.slowc = vcat(output.slowc, reshape((soil.slowc .- μ.slowc) ./ (σ.slowc .- μ.slowc), (1, :)))
 
     output.swc = vcat(output.swc, reshape((soil.swc .- μ.swc) ./ (σ.swc .- μ.swc), (1, :)))
-
 end
 
 function output_finetune!(output::Output,
@@ -34,19 +33,4 @@ function output_finetune!(output::Output,
     output.reco = vcat(output.reco, reshape((crop.resp .+ Zygote.dropgrad(photos.rd) .+ soil.rh .- μ.reco) ./ (σ.reco .- μ.reco), (1, :)))
     output.growing_mask = vcat(output.growing_mask, reshape(crop.isgrowing, (1, :)))
     output.swc = vcat(output.swc, reshape((soil.swc .- μ.swc) ./ (σ.swc .- μ.swc), (1, :)))
-
-end
-
-
-function output_yield!(output::Output,
-                       crop::Crop,
-                       day
-)
-
-    output.stoc = vcat(output.stoc, reshape(crop.stoc, (1, :)))
-
-    if day == 365
-        output.yield = vcat(output.yield, reshape(max.(crop.yield, 0.0f0), (1, :)))
-    end
-
 end
