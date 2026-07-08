63844139a39a8a25a51a3a3b1021a4783bd2feee
yunan-l <yunan2.lin@gmail.com>
2026-07-08 16:18:03 +0200

    update

--- src/processes/climate/readclimate.jl
+++ src/processes/climate/readclimate.jl
@@ -13,9 +13,12 @@ function readclimate!(climate::NamedTuple,
     dailyWeather.swr = climate.sw[day, :]
     dailyWeather.lwr = climate.lw[day, :]
     dailyWeather.temp_n = climate.temp_n[day, :]
-    # dailyWeather.prec_n = climate.prec_n[day, :]
+    dailyWeather.tmax_n = climate.tmax_n[day, :]
+    dailyWeather.tmin_n = climate.tmin_n[day, :]
+    dailyWeather.prec_n = climate.prec_n[day, :]
     dailyWeather.swr_n = climate.sw_n[day, :]
     dailyWeather.lwr_n = climate.lw_n[day, :]
+    dailyWeather.vpd_n = climate.vpd_n[day, :]
     dailyWeather.annual_co2 = ppm2Pa(climate.co2[[div(day-1, 365) + 1]])
 
 end
@@ -31,9 +34,12 @@ function readclimate!(climate::NamedTuple,
     dailyWeather.swr = climate.sw[day, :]
     dailyWeather.lwr = climate.lw[day, :]
     dailyWeather.temp_n = climate.temp_n[day, :]
-    # dailyWeather.prec_n = climate.prec_n[day, :]
+    dailyWeather.tmax_n = climate.tmax_n[day, :]
+    dailyWeather.tmin_n = climate.tmin_n[day, :]
+    dailyWeather.prec_n = climate.prec_n[day, :]
     dailyWeather.swr_n = climate.sw_n[day, :]
     dailyWeather.lwr_n = climate.lw_n[day, :]
+    dailyWeather.vpd_n = climate.vpd_n[day, :]
     dailyWeather.daily_co2 = ppm2Pa(CO2[day, :])
     
 end
